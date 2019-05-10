d16d74a7051b80679a71ccd9fd98c4636a69f364
Leon Shaner <leon@isylum.org>
2019-05-10 16:54:40 -0400

    Added opencv2/imgproc/types_c.h
    Would not compile on Linux (Raspian) without this.
    The change should be harmless for all platforms, since a nearby imgproc.hpp was already included.

--- laucaltagglobject.h
+++ laucaltagglobject.h
@@ -23,6 +23,7 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgproc/types_c.h"
 
 #include "laumemoryobject.h"
 
